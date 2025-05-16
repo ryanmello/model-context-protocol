8cb479184f00a13bd92d826177358377232d2f30
Ryan Mello <ryanmello897@gmail.com>
2025-05-16 16:50:19 -0700

    setup virtual enviornment

--- 0_simple_llm.py
+++ 0_simple_llm.py
@@ -1,1 +1,13 @@
-print("Hello world.")
+import os
+
+import anthropic
+from dotenv import load_dotenv
+
+load_dotenv()
+
+print("hello world")
+
+# client = anthropic.Anthropic(
+#     api_key=os.environ.get("ANTHROPIC_API_KEY")
+# )
+
